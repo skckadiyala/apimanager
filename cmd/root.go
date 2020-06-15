699e594ec91bf86180d0c7df0ff977d37d3758bd
Suman K C Kadiyala <skchkadiyala@axway.com>
2020-06-15 08:52:17 -0700

    Included version flag

--- cmd/root.go
+++ cmd/root.go
@@ -30,9 +30,12 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "apimanager",
-	Short: "apimanager manages apimanager instance",
-	Long:  `apimanager controls and manages API Manager resources.`,
+	Use:     "apimanager",
+	Short:   "apimanager manages apimanager instance",
+	Long:    `apimanager controls and manages API Manager resources.`,
+	Version: "2.0.0",
+	// Run: func(cmd *cobra.Command, args []string) {
+	// },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,7 +50,6 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
@@ -56,7 +58,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
